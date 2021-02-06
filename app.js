9fa9ba80ff82178f191aa5ed3525f425b08276d8
Aakash Jadhav <33982132+aakash-jadhav@users.noreply.github.com>
2021-02-07 01:12:27 +0530

    implemented winning logic

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import checkWinner, { checkDraw } from "./logic.js";
+
 //true = O, false =X
 let currentPlayer = true;
 $(".current").html("O");
@@ -5,11 +7,12 @@ $(".current").html("O");
 //new game
 $(".start").click(function () {
   currentPlayer = true;
-  $(".current").html("O");
   $(".block").removeClass("yellow red");
   $(".block").html("");
   $(".block").addClass("default");
   $(".block").css("pointer-events", "");
+  $("strong").html("Current player: <span class = current></span>");
+  $(".current").html("O");
 });
 
 //click on purple blocks
@@ -19,10 +22,23 @@ $(".default").click(function () {
   $(this).addClass(currentPlayer ? "yellow" : "red");
   $(this).css("pointer-events", "none");
 
+  if (checkWinner(currentPlayer)) {
+    updateStatus(currentPlayer);
+    return;
+  }
+
+  checkDraw();
   togglePlayer();
 });
 
 function togglePlayer() {
   currentPlayer = !currentPlayer;
   $(".current").html(currentPlayer ? "O" : "X");
+}
+
+//update status after victory
+function updateStatus() {
+  let winner = currentPlayer ? "O" : "X";
+  console.log("winner is ", winner);
+  $(".default").css("pointer-events", "none");
 }
---
+++ logic.js
@@ -0,0 +1,27 @@
+//wining logic
+
+let combinations = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [1, 4, 7],
+  [2, 5, 8],
+  [3, 6, 9],
+  [1, 5, 9],
+  [3, 5, 7],
+];
+
+export default function checkWinner(currentPlayer) {
+  let className = currentPlayer ? "yellow" : "red";
+  return combinations.some((combination) => {
+    return combination.every((index) => {
+      return $(`#${index}`).hasClass(className);
+    });
+  });
+}
+
+export function checkDraw() {
+  if ($(".block").hasClass("default")) return;
+  console.log("Match draw");
+  $(".alert").html("The game is draw");
+}
