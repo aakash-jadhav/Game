60f7fa4c616e9cf006ce8cb0d28ae32df92300c6
Aakash Jadhav <33982132+aakash-jadhav@users.noreply.github.com>
2021-02-06 20:45:04 +0530

    Color change on click

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
-console.log("Hello");
+//true = O, false =X
+let currentPlayer = true;
+$(".current").html("O");
 
-$(".default").click(function (e) {
-  console.log("Button clicked");
-  //   $("default").removeClass(".default");
-  //   $("default").addClass("primary");
+//new game
+$(".start").click(function () {
+  currentPlayer = true;
+  $(".current").html("O");
+  $(".block").removeClass("yellow red");
+  $(".block").html("");
+  $(".block").addClass("default");
+  $(".block").css("pointer-events", "");
 });
+
+//click on purple blocks
+$(".default").click(function (e) {
+  $(this).removeClass("default");
+  $(this).html(currentPlayer ? "O" : "X");
+  $(this).addClass(currentPlayer ? "yellow" : "red");
+  $(this).css("pointer-events", "none");
+  //$(this).attr("disabled", true);
+  togglePlayer();
+});
+
+function togglePlayer() {
+  currentPlayer = !currentPlayer;
+  $(".current").html(currentPlayer ? "O" : "X");
+}
