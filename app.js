bbdc56b33212462abaebea57b28c5417f0e644a0
Aakash Jadhav <33982132+aakash-jadhav@users.noreply.github.com>
2021-02-06 17:19:02 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,7 @@
+console.log("Hello");
+
+$(".default").click(function (e) {
+  console.log("Button clicked");
+  //   $("default").removeClass(".default");
+  //   $("default").addClass("primary");
+});
